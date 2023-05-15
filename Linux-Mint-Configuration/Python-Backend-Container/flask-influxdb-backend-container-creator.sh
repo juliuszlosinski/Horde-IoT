95c6f0f92f725222fd7c86422f24219d77837dba
Juliusz Losinski <72278818+juliuszlosinski@users.noreply.github.com>
2023-05-15 13:09:55 +0200

    Fix

--- Linux-Mint-Configuration/Python-Backend-Container/flask-influxdb-backend-container-creator.sh
+++ Linux-Mint-Configuration/Python-Backend-Container/flask-influxdb-backend-container-creator.sh
@@ -8,10 +8,6 @@ echo "2. Starting Flask-InfluxDB backend container..."
 docker run -it -p 5555:5555 -d --name python_backend flask-influxdb-app
 echo "2. Flask-InfluxDB backend container started!"
 
-echo "3. Entering python_backend shell..."
-docker exec -it python_backend /bin/sh
-echo "3. Entered python backend shell!"
-
-echo "4. Starting python_backend shell..."
-flask --app flask_influxdb_backend run -p 5555
-echo "5. Pytho backend shell stopped!"
+echo "3. Starting Flask-InfluxDB python backend..."
+docker exec -it python_backend flask --app flask_influxdb_backend run -p 5555
+echo "4. Flask-InfluxDB python backend finished working!"
