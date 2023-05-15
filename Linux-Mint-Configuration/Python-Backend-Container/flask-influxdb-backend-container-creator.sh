f31e02fe82e9d14d38a094c51bf94fc7769484de
Juliusz Losinski <72278818+juliuszlosinski@users.noreply.github.com>
2023-05-15 14:55:28 +0200

    Fix

--- Linux-Mint-Configuration/Python-Backend-Container/flask-influxdb-backend-container-creator.sh
+++ Linux-Mint-Configuration/Python-Backend-Container/flask-influxdb-backend-container-creator.sh
@@ -5,9 +5,9 @@ docker build -t flask-influxdb-app .
 echo "1. Flask-InfluxDB image is created!"
 
 echo "2. Starting Flask-InfluxDB backend container..."
-docker run -it -p 5555:5555 -d --name python_backend flask-influxdb-app
+docker run -it -p 4500:4500 -d --name python_backend flask-influxdb-app
 echo "2. Flask-InfluxDB backend container started!"
 
 echo "3. Starting Flask-InfluxDB python backend..."
-docker exec -it python_backend flask --app flask_influxdb_backend run -p 5555
+docker exec -it python_backend flask --app flask_influxdb_backend run -p 4500
 echo "4. Flask-InfluxDB python backend finished working!"
--- Linux-Mint-Configuration/Python-Backend-Container/flask_influxdb_backend.py
+++ Linux-Mint-Configuration/Python-Backend-Container/flask_influxdb_backend.py
@@ -78,4 +78,4 @@ def add_measurement():
     return '', 204
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
