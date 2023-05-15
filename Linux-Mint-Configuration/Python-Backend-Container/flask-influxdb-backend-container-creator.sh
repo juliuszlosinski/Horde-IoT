69f1f8bfd882780b39ddae525086c55ca3f2403e
Juliusz Losinski <72278818+juliuszlosinski@users.noreply.github.com>
2023-05-15 12:59:33 +0200

    Flask + Docker

--- Linux-Mint-Configuration/Python-Backend-Container/flask-influxdb-backend-container-creator.sh
+++ Linux-Mint-Configuration/Python-Backend-Container/flask-influxdb-backend-container-creator.sh
@@ -5,5 +5,8 @@ docker build -t flask-influxdb-app .
 echo "1. Flask-InfluxDB image is created!"
 
 echo "2. Starting Flask-InfluxDB backend container..."
-docker run -it -p 4000:4000 -d --name python_backend flask-influxdb-app
+docker run -it -p 5555:5555 -d --name python_backend flask-influxdb-app
 echo "2. Flask-InfluxDB backend container started!"
+
+docker exec -it python_backend /bin/sh
+flask --app flask_influxdb_backend run -p 5555:5555
